50220506d803c7cc0e34a63214feb333108d97bd
Juntakan <59030139@kmitl.ac.th>
2017-09-19 20:18:26 +0700

    2

--- week6/week6/Program.cs
+++ week6/week6/Program.cs
@@ -7,18 +7,15 @@ namespace week6
     {
         static void Main()
         {
-            // Create web client.
-    	WebClient client = new WebClient();
-    	// Download string.
-    	string value = client.DownloadString("http://www.kmitl.ac.th");
-    	// Write values.
-    	Console.WriteLine("--- WebClient result ---");
-    	Console.WriteLine(value.Length);
-    	Console.WriteLine(value);
-    	StreamWriter outtxt = new StreamWriter("webload.html");
-    	outtxt.Write(value);
-    	outtxt.Dispose();
-
+            sayHello("Hello World.");
+            Console.ReadLine();
         }
+
+        static void sayHello(string str)
+        {
+            Console.WriteLine(str);
+        }
+
     }
 }
+
